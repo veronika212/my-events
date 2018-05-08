2c1c887be75a9e360b937a5caba0a65083fc9c9e
veronika <veronika212.vyhlidalova@gmail.com>
2018-05-08 21:20:03 +0200

    using the grid system in the component event-detail

--- src/pages/event-detail/EventDetail.tsx
+++ src/pages/event-detail/EventDetail.tsx
@@ -47,78 +47,80 @@ class EventDetail extends Component<EventDetailProps> {
     }
 
     return (
-      <div className="clearfix event-detail">
-        <div className="user-tile">{this.renderUserTile()}</div>
+      <div className="container-fluid">
+        <div className="event-detail">
+          <div className="row event-detail__content-wrapper">
+            <div className="d-none d-lg-block col-lg-3 user-tile">{this.renderUserTile()}</div>
+            <div className="col-xs-12 col-lg-6 event-detail__content">
+              <img className="event-detail__image" src={eventDetail.image} alt="pictures" />
 
-        <div className="event-detail__content">
-          <img className="event-detail__image" src={eventDetail.image} alt="pictures" />
+              <div className="event-detail__border">
+                <div>
+                  <div className="basic-info clearfix">
+                    <div className="basic-info__start-date">
+                      <p className="basic-info__number-day">
+                        {format(eventDetail.startDate, 'DD MMM')}
+                      </p>
+                      <p>{format(eventDetail.startDate, 'dd')}</p>
+                    </div>
 
-          <div className="event-detail__border">
-            <div>
-              <div className="basic-info clearfix">
-                <div className="basic-info__start-date">
-                  <p className="basic-info__number-day">
-                    {format(eventDetail.startDate, 'DD MMM')}
-                  </p>
-                  <p>{format(eventDetail.startDate, 'dd')}</p>
+                    <div className="basic-info__place-event">
+                      <p className="basic-info__title-event">{eventDetail.name}</p>
+                      <p>{eventDetail.county}</p>
+                    </div>
+                  </div>
+
+                  <div className="item-participation">
+                    <Button label={`Going ${eventDetail.going}`} />
+                    <Button label={`Interest ${eventDetail.interested}`} />
+                    <Button
+                      className="item-participation__last-button"
+                      label={`Like ${eventDetail.likes}`}
+                    />
+                  </div>
                 </div>
+                <hr />
 
-                <div className="basic-info__place-event">
-                  <p className="basic-info__title-event">{eventDetail.name}</p>
-                  <p>{eventDetail.county}</p>
+                <div>
+                  <div className="information">
+                    <p className="information__dark-text">{`${format(
+                      eventDetail.endDate,
+                      'dddd'
+                    )} ${'from'} ${format(eventDetail.startDate, 'HH:mm')} ${'to'} ${format(
+                      eventDetail.endDate,
+                      'dddd'
+                    )} ${format(eventDetail.endDate, 'HH:mm')}`}</p>
+
+                    <p className="information__light-text">{`${'from'} ${format(
+                      eventDetail.startDate,
+                      'DD MMMM'
+                    )} ${'to'} ${format(eventDetail.endDate, 'DD MMMM')}`}</p>
+                  </div>
+
+                  <div className="information">
+                    <p className="information__dark-text">{eventDetail.address.place}</p>
+                    <p className="information__light-text">{eventDetail.category}</p>
+                  </div>
+
+                  <div className="information__address">
+                    <span className="information__dark-text">Address</span>
+                    <p className="information__light-text">{`${eventDetail.address.street}, ${
+                      eventDetail.address.city
+                    } ${eventDetail.address.zipCode}, ${eventDetail.address.state}`}</p>
+                  </div>
                 </div>
-              </div>
-
-              <div className="item-participation">
-                <Button label={`Going ${eventDetail.going}`} />
-                <Button label={`Interest ${eventDetail.interested}`} />
-                <Button
-                  className="item-participation__last-button"
-                  label={`Like ${eventDetail.likes}`}
-                />
               </div>
             </div>
-            <hr />
-
-            <div>
-              <div className="information">
-                <p className="information__dark-text">{`${format(
-                  eventDetail.endDate,
-                  'dddd'
-                )} ${'from'} ${format(eventDetail.startDate, 'HH:mm')} ${'to'} ${format(
-                  eventDetail.endDate,
-                  'dddd'
-                )} ${format(eventDetail.endDate, 'HH:mm')}`}</p>
-
-                <p className="information__light-text">{`${'from'} ${format(
-                  eventDetail.startDate,
-                  'DD MMMM'
-                )} ${'to'} ${format(eventDetail.endDate, 'DD MMMM')}`}</p>
-              </div>
-
-              <div className="information">
-                <p className="information__dark-text">{eventDetail.address.place}</p>
-                <p className="information__light-text">{eventDetail.category}</p>
-              </div>
-
-              <div className="information__address">
-                <span className="information__dark-text">Address</span>
-                <p className="information__light-text">{`${eventDetail.address.street}, ${
-                  eventDetail.address.city
-                } ${eventDetail.address.zipCode}, ${eventDetail.address.state}`}</p>
-              </div>
-            </div>
+            <div className="col-lg-3 user-tile">{this.renderUserTile()}</div>
           </div>
+
+          <div className="description-box">
+            <p className="description-box__title">Description event </p>
+            <p>{eventDetail.description}</p>
+          </div>
+
+          <div>{this.renderCommentsTile()}</div>
         </div>
-
-        <div className="user-tile">{this.renderUserTile()}</div>
-
-        <div className="description-box">
-          <p className="description-box__title">Description event </p>
-          <p>{eventDetail.description}</p>
-        </div>
-
-        <div>{this.renderCommentsTile()}</div>
       </div>
     );
   }
